1c9fbe54ca8e61fba87ece5f233be652aec5557b
Administrator <215441036@qq.com>
2018-10-26 15:35:56 +0800

    cc

---
+++ modle01/src/com/shiro.java
@@ -0,0 +1,8 @@
+package com;
+
+public class shiro {
+    public static void main(String[] args) {
+        System.out.println("jjjjjj");
+
+    }//单上试试按上大声道阿萨德
+}
---
+++ src/com/test/Hello.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        if (args == null) {
+            String s = new String();
+        }
+
+//        System.out.println("HelloWORLD");/**/
+    }
+}
